7b9e4b8692e2ebaa75bfb3c1834978b998242c90
Vro2000 <veronique.urbino@hotmail.fr>
2024-04-24 17:29:58 +0200

    Etape 3 : ajoutez les bullet points

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,11 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+    { "image":"assets/images/slideshow/slide1.jpg", "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>" },
+    { "image":"assets/images/slideshow/slide2.jpg", "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>" },
+    { "image":"assets/images/slideshow/slide3.jpg", "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>" },
+    { "image":"assets/images/slideshow/slide4.png", "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>" }
+];
+
+/*********************************** */
 // Sélectionne les flèches à l'intérieur de la div #banner
 const leftArrow = document.querySelector('#banner .arrow_left');
 const rightArrow = document.querySelector('#banner .arrow_right');
@@ -36,6 +26,30 @@ function fonct_rightArrowClick() {
 	console.log("Flèche droite cliquée");
 	alert("Flèche droite cliquée");
 }
+/************************************ */
+
+// ajoute dynamiquement les span.dot au html, en comptant les images du tableau 'slides'
+function fonct_createDotSpan() {
+    slides.forEach((_, index) => { 							// pour chaque entrée dans le tableau 'slides'
+		const divDots = document.querySelector('#banner .dots'); 	// Sélectionne la div qui a id=banner class=.dots
+        const spanDot = document.createElement('span'); 	// crée un span
+        spanDot.classList.add('dot'); 						// ajoute la class .dot au span
+		
+        if (index === 0) {									// Ajoute la classe .dot_selected au premier spanDot
+            spanDot.classList.add('dot_selected');
+        }
+
+        divDots.appendChild(spanDot); 						// ajoute le spanDot dans divDots
+    });
+}
+
+fonct_createDotSpan(); // ne pas oublier de Lancer la fonction ;)
+
+
+
+
+
+
 
 
 
