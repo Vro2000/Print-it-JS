39c9096db2e1a01ea36cc75d3fa883e5bfbc5adf
Vro2000 <veronique.urbino@hotmail.fr>
2024-04-24 14:53:02 +0200

    Etape 2 : Ajout Event listeners sur les flèches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,26 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+// Sélectionne les flèches à l'intérieur de la div #banner
+const leftArrow = document.querySelector('#banner .arrow_left');
+const rightArrow = document.querySelector('#banner .arrow_right');
+
+// Ajoute des écouteurs d'événements
+leftArrow.addEventListener('click', fonct_leftArrowClick);
+	console.log("écouteur sur flèche gauche");
+rightArrow.addEventListener('click', fonct_rightArrowClick);
+	console.log("écouteur sur flèche droite");
+
+ // Fonctions pour gérer les clics sur les flèches
+ function fonct_leftArrowClick() {
+	console.log("Flèche gauche cliquée");
+	alert("Flèche gauche cliquée");
+}
+
+function fonct_rightArrowClick() {
+	console.log("Flèche droite cliquée");
+	alert("Flèche droite cliquée");
+}
+
+
+
